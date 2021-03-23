944e306a293f93c3a1a3d09ad48550a6e8181773
D34DPlayer <d34dplayer@protonmail.com>
2021-03-23 12:57:54 +0100

    DB Model User
    #54

--- api/models.py
+++ api/models.py
@@ -1,28 +1,12 @@
-from db import db, metadata
-from db.sqlalchemy import Table, Column, Integer, String
+from db import metadata
+from db import sqlalchemy
 
 
-""" EXAMPLE
-users = Table(
+users = sqlalchemy.Table(
     "users",
     metadata,
-    Column("id", Integer, primary_key=True),
-    Column("first_name", String),
-    Column("last_name", String),
-    Column("age", Integer),
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("username", sqlalchemy.String, unique=True),
+    sqlalchemy.Column("email", sqlalchemy.String),
+    sqlalchemy.Column("hashed_password", sqlalchemy.String),
 )
-
-
-class User:
-    @classmethod
-    async def get(cls, id):
-        query = users.select().where(users.c.id == id)
-        user = await db.fetch_one(query)
-        return user
-
-    @classmethod
-    async def create(cls, **user):
-        query = users.insert().values(**user)
-        user_id = await db.execute(query)
-        return user_id
-"""
