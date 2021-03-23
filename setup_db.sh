c70b702ceefb62a02c6b5aea550b080615349799
D34DPlayer <d34dplayer@protonmail.com>
2021-03-23 14:39:10 +0100

    script config initiale db

---
+++ setup_db.sh
@@ -0,0 +1,5 @@
+docker-compose -p devweb --file docker-compose.dev.yml run api alembic upgrade head
+
+docker-compose -p devweb --file docker-compose.dev.yml run db psql -U postgres -c "INSERT INTO users (username, email, hashed_password) VALUES ('admin', 'admin@boucherie.tk', '$2b$12$FmDCDxROtlOb8vNdYSf0nOcoKv67ez1q2w4mWC.DeOSnm5JwpdpbW');"
+
+echo "User: admin\nPwd: superpassword"
