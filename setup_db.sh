b38ca2c3420602a564ce1595f382c83204bdf3b6
D34DPlayer <d34dplayer@protonmail.com>
2021-04-04 17:43:04 +0200

    Vue enlever TS
    Pcq cyril va mourir sinon

--- setup_db.sh
+++ setup_db.sh
@@ -1,5 +1,7 @@
-docker-compose -p devweb run api alembic upgrade head
+docker-compose -p devweb up -d;
 
-docker-compose -p devweb run db psql -U postgres -c "INSERT INTO users (username, email, hashed_password) VALUES ('admin', 'admin@boucherie.tk', '$2b$12$FmDCDxROtlOb8vNdYSf0nOcoKv67ez1q2w4mWC.DeOSnm5JwpdpbW');"
+docker-compose -p devweb exec --workdir /api api alembic upgrade head
+
+docker-compose -p devweb exec db psql -U postgres -c "INSERT INTO users (username, email, hashed_password) VALUES ('admin', 'admin@boucherie.tk', '$2b$12$FmDCDxROtlOb8vNdYSf0nOcoKv67ez1q2w4mWC.DeOSnm5JwpdpbW');"
 
 echo "User: admin\nPwd: superpassword"
