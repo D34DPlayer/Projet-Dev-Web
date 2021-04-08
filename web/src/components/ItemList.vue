fef8ecdf7ffe4aefe485d6e2bece58824ae4cdc0
cyrilGrandjean <64021617+cyrilGrandjean@users.noreply.github.com>
2021-04-08 14:28:52 +0200

    update html
    création de liste produit et des fichier vue lier a cela

---
+++ web/src/components/ItemList.vue
@@ -0,0 +1,39 @@
+<template>
+  <b-col lg="3" md="4" sm="6" cols="12" class="my-3">
+    <b-card
+        :img-src="product.src[0]"
+        :img-alt="product.name"
+        img-bottom
+        tag="article"
+    >
+      <b-card-title>{{ product.name }}</b-card-title>
+      <b-card-sub-title>{{ product.categorie }}</b-card-sub-title>
+      <b-card-text>{{ product.prix }}</b-card-text>
+    </b-card>
+  </b-col>
+</template>
+
+<script>
+export default {
+  name: "ItemList",
+  props: ["product"],
+  data() {
+    return {
+      size: "",
+    };
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.card-text {
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  padding: 1rem;
+  background-color: rgba(255, 255, 255, 0.75);
+  border-radius: 0.25rem 0 0 0;
+}
+
+
+</style>
