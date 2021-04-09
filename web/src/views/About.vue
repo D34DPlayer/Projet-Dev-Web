29051479dd2263b9b158a000e6b28058f19c94ad
cyrilGrandjean <64021617+cyrilGrandjean@users.noreply.github.com>
2021-04-09 19:10:24 +0200

    Création vue About
    #65
    Création de About.vue.
    Création du template.
    Création du style.
    Ajout d'une icon pour les puces de liste.

--- web/src/views/About.vue
+++ web/src/views/About.vue
@@ -1,5 +1,84 @@
 <template>
-  <div class>
-    <h1>This is an about page</h1>
-  </div>
+  <b-container>
+    <h1>UN BOUCHER TOUJOURS PASSIONNÉ PAR LA LICORNE</h1>
+    <p class="sous-titre">pour offrir le meilleur</p>
+    <p>Vous recherchez un <em>boucher à Leuze (Namur)</em>, passionné et toujours prêt à satisfaire vos demandes ?
+      Michael pratique
+      ce métier depuis plus de 21 ans et c’est en compagnie d’Emilie qu’il vous accueille au sein de la boucherie
+      artisanale Vangeebergen. Nous vous servons tous les jours (à l’exception du mardi) avec le sourire et sommes à
+      l’écoute de vos suggestions, appréciations et remarques. La nature nous offre des trésors, nous les préparons avec
+      un savoir-faire authentique pour votre plus grande satisfaction.</p>
+
+    <h2>Nos spécialités</h2>
+    <p>Nous allions notre savoir-faire et nos recettes traditionnels aux produits de qualité proposés par des <em>fermes
+      de la
+      région</em> en qui nous avons toute confiance. Ainsi, notre assortiment de viandes de <em>bœuf</em> provient des fermes
+      locales,
+      tandis que certains fromages et produits laitiers sont fournis par la ferme de Warisoulx. En favorisant les
+      circuits
+      courts, nous privilégions la qualité en provenance d’élevages de proximité pour parfaire nos préparations maison.
+    </p>
+
+    <h2>Nos étals</h2>
+    <p>En toute saison, vous trouverez à la Boucherie Vangeebergen des viandes fraîches et savoureuses :
+    <ul>
+      <li>boeuf ;</li>
+      <li>veau ;</li>
+      <li>porc Duroc d’Olives ;</li>
+      <li>agneau ;</li>
+      <li>volaille ;</li>
+      <li>gibier (en hiver) ;</li>
+      <li>viandes pour barbecue (en été).</li>
+    </ul>
+    Au rayon charcuterie, nous proposons des préparations maison, dont le pâté du chef (pâté de campagne), mais aussi un
+    assortiment plus traditionnel (jambon de porc Duroc d’Olives, salami, rosette de Lyon, saucisson de jambon). Pour
+    les plus pressés, nous préparons chaque semaine des plats traiteur artisanaux à emporter et faciles à réchauffer.
+    Toutes nos préparations sont réalisées dans le respect des règles strictes d’hygiène et de la sécurité de la chaîne
+    alimentaire.
+    </p>
+
+    <h2>Contactez votre boucher</h2>
+    <p>Nous sommes ouverts du lundi au dimanche, à l’exception du mardi. Situés à Leuze (Namur), nous sommes facilement
+      accessibles depuis Éghezée, Bolinne, Fernelmont, La Bruyère ou Émines. Vous souhaitez commander, en savoir
+      davantage
+      sur nos produits ou réserver votre sandwich ? Prenez contact avec la boucherie. Nous vous répondrons avec le
+      sourire.</p>
+  </b-container>
 </template>
+
+<style scoped lang="scss">
+h1 {
+  color: var(--primary);
+  text-align: center;
+  font-weight: 200;
+}
+
+p.sous-titre {
+  text-align: center;
+  font-weight: 100;
+  font-size: 1.5rem;
+  letter-spacing: .3rem;
+}
+
+h2 {
+  font-weight: 300;
+  font-size: 1.5rem;
+}
+
+em {
+  font-style: normal;
+  font-weight: 400;
+}
+
+p {
+  font-weight: 300;
+  letter-spacing: .2rem;
+  line-height: 1.5rem;
+}
+
+li {
+  list-style: none;
+  background: url('~@/assets/list-icon.png') no-repeat 0 7px;
+  padding-left: 20px;
+}
+</style>
