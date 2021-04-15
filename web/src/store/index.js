b88fc3b6db1e9a9afc5cc40f28878a12775a6353
D34DPlayer <d34dplayer@protonmail.com>
2021-04-15 16:39:29 +0200

    Vuex horaire

--- web/src/store/index.js
+++ web/src/store/index.js
@@ -3,10 +3,11 @@ import Vuex from "vuex";
 import createPersistedState from "vuex-persistedstate";
 
 import users from "./users";
+import horaire from "./horaire";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   plugins: [createPersistedState()],
-  modules: { users },
+  modules: { users, horaire },
 });
