5faa6ce9990b3c6377df8c585f3fca509a7d032f
cyrilGrandjean <64021617+cyrilGrandjean@users.noreply.github.com>
2021-04-18 20:26:41 +0200

    frontend api get products

--- web/src/store/products.js
+++ web/src/store/products.js
@@ -4,9 +4,14 @@ const state = () => ({
   endpoints: {
     products: "/api/products",
   },
+  products: [],
 });
 
-const mutations = {};
+const mutations = {
+  updateProduct(state, payload) {
+    state.products = payload;
+  },
+};
 
 const actions = {
   async addProduct({ state, commit, rootState }, data) {
@@ -30,6 +35,23 @@ const actions = {
       if (e.response.status === 401) {
         commit("logout");
       }
+      return e.response;
+    }
+  },
+  async getProducts({ state, commit }) {
+    const url = state.endpoints.products;
+
+    try {
+      let response = await axios(url, {
+        method: "GET",
+        headers: {
+          Accept: "*/*",
+        },
+      });
+      commit("updateProduct", response.data);
+      return response;
+    } catch (e) {
+      console.error(e);
       return e.response;
     }
   },
