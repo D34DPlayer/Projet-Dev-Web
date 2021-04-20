0835213caa68a900a0b3a7fd9a9a495c084c5bcb
de-Pret-Mikael <64020511+de-Pret-Mikael@users.noreply.github.com>
2021-04-20 19:34:18 +0200

    Api visibilité
    #78
    Ajout de la route /product/id/visibility
    modification de l'information visibility de la base de donnée

--- api/routers/products.py
+++ api/routers/products.py
@@ -1,8 +1,8 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 
 from ..app import is_connected
 
-from ..schemas import Product
+from ..schemas import Product, VisibilityModel
 
 
 router = APIRouter(
@@ -22,3 +22,14 @@ async def add_product(product: Product):
 async def get_products():
     """get a list of product."""
     return await Product.get_all()
+
+@router.put("/{id}/visibility", response_model=Product, dependencies=[Depends(is_connected)])
+async def update_product_visibility(id: int, visibility: VisibilityModel):
+    """Updates the visibility of a product"""
+    product = await Product.get(id)
+    if not product:
+        raise HTTPException(status=status.HTTP_404_NOT_FOUND, detail="The product doesn't exist.")
+    if visibility.visibility:
+        return await Product.show(id)
+    else:
+        return await Product.hide(id)
