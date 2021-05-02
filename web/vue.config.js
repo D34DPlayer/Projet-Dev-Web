caa54b485bc998baf0813bd38b28a63d3b7b5a8b
D34DPlayer <d34dplayer@protonmail.com>
2021-05-02 17:46:19 +0200

    Sitemap + robots.txt

--- web/vue.config.js
+++ web/vue.config.js
@@ -1,10 +1,16 @@
 const path = require("path");
+const fs = require("fs");
+
 const PrerenderSpaPlugin = require("prerender-spa-plugin");
+const SitemapPlugin = require("sitemap-webpack-plugin").default;
+const RobotstxtPlugin = require("robotstxt-webpack-plugin");
+
+const routerPaths = ["/", "/about", "/login", "/products", "/contact"];
 
 const productionPlugins = [
   new PrerenderSpaPlugin({
     staticDir: path.join(__dirname, "dist"),
-    routes: ["/", "/about", "/contact"],
+    routes: routerPaths,
     renderer: new PrerenderSpaPlugin.PuppeteerRenderer({
       inject: {
         foo: "bar",
@@ -12,6 +18,19 @@ const productionPlugins = [
       headless: true,
       renderAfterDocumentEvent: "render-event",
     }),
+  }),
+  new SitemapPlugin({
+    base: `https://${process.env.DOMAIN_NAME}`,
+    paths: routerPaths,
+    options: {
+      fileName: "sitemap.xml",
+      lastMod: true,
+      changeFreq: "daily",
+    },
+  }),
+  new RobotstxtPlugin({
+    sitemap: `https://${process.env.DOMAIN_NAME}/robots.txt`,
+    host: `https://${process.env.DOMAIN_NAME}`,
   }),
 ];
 
@@ -24,6 +43,11 @@ module.exports = {
   configureWebpack: (config) => {
     if (process.env.NODE_ENV === "production") {
       config.plugins.push(...productionPlugins);
+
+      fs.writeFileSync(
+        "dist/robots.txt",
+        `Sitemap: https://${process.env.DOMAIN_NAME}/sitemap.xml`
+      );
     }
   },
 };
