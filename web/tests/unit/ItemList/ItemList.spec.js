cff032af376cb2f44e4308c124ecab6717ee779f
cyrilGrandjean <64021617+cyrilGrandjean@users.noreply.github.com>
2021-05-04 19:04:44 +0200

    Test Unitaire ItemList.spec.js END

--- web/tests/unit/ItemList/ItemList.spec.js
+++ web/tests/unit/ItemList/ItemList.spec.js
@@ -1,31 +1,68 @@
-import { shallowMount } from "@vue/test-utils";
-import ItemList from "@/components/ItemList.vue"
+import { mount, RouterLinkStub } from "@vue/test-utils";
+import Vuex from "vuex";
+import Vue from "vue";
+import { BootstrapVue, BootstrapVueIcons } from "bootstrap-vue";
+import VueRouter from "vue-router";
+import ItemList from "@/components/ItemList";
+Vue.use(BootstrapVue);
+Vue.use(BootstrapVueIcons);
+Vue.use(Vuex);
+Vue.use(VueRouter);
 
-describe("ItemList.spec.js", () =>{
-    let cmp;
+const component = () => {
+  return mount(ItemList, {
+    propsData: {
+      product: {
+        "id": 4,
+        "name": "Banane",
+        "categorie": "Fruit",
+        "description": "c'est un fruit",
+        "photos": ["/images/products/4/alpaga.jpg"],
+        "price": 50.0,
+        "promo_price": 25.0,
+        "price_type": "/kilo",
+        "visibility": true
+      }
+    },
+    stubs: { RouterLink: RouterLinkStub }
+  })
+}
 
-    beforeEach(() => {
-       cmp = shallowMount(ItemList, {
-           data: {"id":4,
-               "name":"Bananne",
-               "categorie":"Fruit",
-               "description":"c'est un fruit",
-               "photos":["/images/products/4/alpaga.jpg"],
-               "price":50.0,
-               "promo_price":25.0,
-               "price_type":"/kilo",
-               "visibility":true}
-       });
-    });
+describe('ItemList.spec.js', () => {
+  it("Test text de la carte.", () => {
+    const wrapper = component();
+    expect(wrapper.text()).toBe("Banane Fruit 25.00€");
+  });
 
-    it("equals name to Bananne", () => {
-       expect(cmp.vm.name).toEqual("Bananne");
-    });
+  it("Test de click sur une carte", async () => {
+    const wrapper = component();
+    await wrapper.trigger("click");
+    await Vue.nextTick();
+    expect(wrapper.findComponent(RouterLinkStub).props().to).toBe('/products/4')
+  });
 
-    it("equals categorie to Fruit", () => {
-       expect(cmp.vm.categorie).toEqual("Fruit");
+  it("Test Photo Source Image", () => {
+    const wrapper = component();
+    expect(wrapper.vm.imageFound.backgroundImage).toBe("url(/images/products/4/alpaga.jpg)");
+  });
+
+  it("Test No Photo Source Image",() => {
+    const wrapper = mount(ItemList, {
+      propsData: {
+        product: {
+          "id": 4,
+          "name": "Banane",
+          "categorie": "Fruit",
+          "description": "c'est un fruit",
+          "photos": [],
+          "price": 50.0,
+          "promo_price": 25.0,
+          "price_type": "/kilo",
+          "visibility": true
+        }
+      },
+      stubs: { RouterLink: RouterLinkStub }
     });
-    it("has the expected html structure", () => {
-        expect(cmp.element).toMatchSnapshot();
-    });
+    expect(wrapper.vm.imageFound).isEmpty;
+  });
 });
