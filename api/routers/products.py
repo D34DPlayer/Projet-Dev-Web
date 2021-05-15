812aa8997788de47414704f7e38e1d9eafbc6e8e
de-Pret-Mikael <64020511+de-Pret-Mikael@users.noreply.github.com>
2021-05-15 16:48:03 +0200

    ajout route detail produit

--- api/routers/products.py
+++ api/routers/products.py
@@ -93,11 +93,20 @@ async def get_products():
     """get a list of product."""
     return await Product.get_all()
 
+@router.get("/{product_id}", response_model=Product)
+async def get_product_id(product_id: int):
+    """get a product by id"""
+    product = await Product.get(product_id)
+    if not product:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No product with that id was found.")
+    return product
+
 
 @router.delete("/{product_id}", response_model=Product, dependencies=[Depends(is_connected)])
 async def delete_product(product_id: int):
     """Delete an existing product."""
     product = await Product.delete(product_id)
+
     if not product:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No product with that id was found.")
 
