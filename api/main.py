55f1290cef84c947aa5b10cf2a9c63bb2bb25d7a
D34DPlayer <d34dplayer@protonmail.com>
2021-05-15 20:31:47 +0200

    API messages

--- api/main.py
+++ api/main.py
@@ -1,11 +1,12 @@
 from .app import app
 
-from .routers import users, horaire, products
+from .routers import users, horaire, products, messages
 
 
 app.include_router(users.router)
 app.include_router(horaire.router)
 app.include_router(products.router)
+app.include_router(messages.router)
 
 
 @app.get("/")
