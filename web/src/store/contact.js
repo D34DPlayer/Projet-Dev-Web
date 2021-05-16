106f566391e76a18c7cb65ba2be1bc9dcc299b65
Mathieu Walravens <m.walravens@students.ephec.be>
2021-05-16 03:07:25 +0200

    Add editContact

--- web/src/store/contact.js
+++ web/src/store/contact.js
@@ -42,7 +42,33 @@ const actions = {
       console.error(e);
       return e.response;
     }
-  }
+  },
+  async editContact({ state, commit, dispatch, rootState }, data) {
+    const url = state.endpoints.contact;
+
+    const AuthStr = "Bearer ".concat(rootState.users.user.token);
+
+    try {
+      let response = await axios(url, {
+        method: "PUT",
+        headers: {
+          Accept: "*/*",
+          Authorization: AuthStr,
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+        data: data,
+      });
+      dispatch("getContact");
+      return response;
+    } catch (e) {
+      console.error(e);
+      if (e.response.status === 401) {
+        commit("users/logout", null, { root: true });
+      }
+      return e.response;
+    }
+  },
 };
 
 export default {
