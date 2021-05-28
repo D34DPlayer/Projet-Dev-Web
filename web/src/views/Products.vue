097324a2b455c12a99695d73c2224e2237abab00
Mathieu Walravens <m.walravens@students.ephec.be>
2021-05-28 16:12:24 +0200

    Search API frontend

--- web/src/views/Products.vue
+++ web/src/views/Products.vue
@@ -2,6 +2,29 @@
   <div class="mx-5">
     <h1 class="text-center">Liste des produits</h1>
     <b-container>
+      <b-form @submit.prevent="search" class="row">
+        <b-col lg="9">
+          <b-form-input
+            id="search"
+            minlength="3"
+            placeholder="Rechercher un produit"
+          >
+          </b-form-input>
+        </b-col>
+        <b-col>
+          <b-button type="submit" variant="primary">Rechercher</b-button>
+        </b-col>
+        <b-col>
+          <b-button
+            type="button"
+            v-if="is_search"
+            @click="clear"
+            variant="secondary"
+          >
+            <BIconX />Clear
+          </b-button>
+        </b-col>
+      </b-form>
       <b-row>
         <ItemList
           v-for="product in products"
@@ -22,10 +45,10 @@
 
 <script>
 import ItemList from "@/components/ItemList.vue";
-import { BPagination } from "bootstrap-vue";
+import { BPagination, BFormInput, BForm, BIconX } from "bootstrap-vue";
 
 export default {
-  components: { ItemList, BPagination },
+  components: { ItemList, BPagination, BForm, BFormInput, BIconX },
   metaInfo: () => ({
     title: "Nos produits",
     meta: [
@@ -36,11 +59,33 @@ export default {
       },
     ],
   }),
+  data() {
+    return {
+      search_data: "",
+      is_search: false,
+    };
+  },
   name: "ProduitList",
   mounted() {
     this.$store.dispatch("products/getProducts", [
       this.$store.state.products.page,
     ]);
+  },
+  methods: {
+    search() {
+      console.log("ALO");
+      const element = document.getElementById("search");
+      let data = element.value;
+
+      this.is_search = true;
+      this.search_data = data;
+      this.$store.dispatch("products/search", [data]);
+    },
+    clear() {
+      this.search_data = "";
+      this.is_search = false;
+      this.$store.dispatch("products/getProducts", [1]);
+    },
   },
   computed: {
     products() {
@@ -59,7 +104,11 @@ export default {
         return this.$store.state.products.page;
       },
       set(value) {
-        this.$store.dispatch("products/getProducts", [value]);
+        if (this.is_search) {
+          this.$store.dispatch("products/search", [this.search_data, value]);
+        } else {
+          this.$store.dispatch("products/getProducts", [value]);
+        }
       },
     },
   },
